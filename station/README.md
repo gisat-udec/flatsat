9d3d73b8939494c72861cbfae143d1317825a4df
qgerman2 <qgerman2@gmail.com>
2024-11-21 16:30:55 -0300

    cambio de receptor a esp32

---
+++ station/README.md
@@ -0,0 +1,13 @@
+# Station
+
+Sketch de Arduino para la ESP32 (LilyGO T-Internet-COM) que recibe paquetes Wi-Fi y retransmite los datos por Ethernet.
+
+### Dependencias
+
+- Arduino IDE 2.3.3
+  
+
+### Hardware
+
+- LilyGO T-Internet-COM (ESP32 Dev Module)
+
--- station/station.ino
+++ station/station.ino
@@ -1,32 +1,30 @@
 #include <WiFi.h>
-#include <WiFiUdp.h>
+#include <WiFiAP.h>
+#include <NetworkUdp.h>
 
 // Configuración Wi-Fi
 #define SSID "station"
 #define PASSWORD "station123"
 IPAddress apIP(10, 0, 0, 1);
 
-// Servidor UDP
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
-WiFiUDP Udp;
+// Configuración servidor UDP
+NetworkUDP udp;
+char packetBuffer[65565];
 unsigned int localPort = 8888;
 
 void setup() {
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP(SSID, PASSWORD);
-  Udp.begin(localPort);
+    Serial.begin(115200);
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+    WiFi.softAP(SSID, PASSWORD);
+    udp.begin(localPort);
 }
 
 void loop() {
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    Serial.printf("Received packet of size %d from %s:%d\n    (to %s:%d)\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort(), Udp.destinationIP().toString().c_str(), Udp.localPort());
-
-    // read the packet into packetBufffer
-    int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    packetBuffer[n] = 0;
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
-  }
+    int packetSize = udp.parsePacket();
+    if (packetSize) {
+        int n = udp.read(packetBuffer, sizeof(packetBuffer));
+        packetBuffer[n] = 0;
+        Serial.println(packetBuffer);
+    }
 }
